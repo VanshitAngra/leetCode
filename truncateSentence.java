8c26c58af2d471019241c274a803850c4c592ae0
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-01 17:25:58 +0530

    addition of truncateString file

---
+++ truncateSentence.java
@@ -0,0 +1,21 @@
+//1816
+import java.util.Scanner;
+
+public class truncateSentence {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        String s=ob.nextLine();
+        int k=ob.nextInt();
+        String[] arr=s.split(" ");
+        String n="";
+        for(int i=0;i<k;i++)
+        {
+            if(i==k-1)
+            n+=arr[i];
+            else
+            n+=arr[i]+" ";
+        }
+        System.out.println(n);
+        ob.close();
+    }
+}

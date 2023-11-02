8a854885dec2c76322c1566572aaf601e386342e
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-02 11:38:25 +0530

    addition of file ugly number

---
+++ ugkynumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ugkynumber {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        while(n>1)
+        {
+            if(n%2==0)
+            n/=2;
+            else if(n%3==0)
+            n/=3;
+            else if(n%5==0)
+            n/=5;
+            else if(n>1)
+            {
+                System.out.println("false");
+                break;
+            }
+        }
+        if(n>0||n==1)
+        System.out.println("true");
+        else
+        System.out.println("false");
+
+        ob.close();
+    }
+}

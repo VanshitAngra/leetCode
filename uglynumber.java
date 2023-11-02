4a4f38525b0a926154c8ca75400b928c1f1b6730
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-02 11:39:57 +0530

    modification in ugly number

--- uglynumber.java
+++ uglynumber.java
@@ -1,6 +1,7 @@
+//263
 import java.util.Scanner;
 
-public class ugkynumber {
+public class uglynumber {
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         int n=ob.nextInt();

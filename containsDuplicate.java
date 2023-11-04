ca6d11b5a6ffef10bc63961b4330da50b32b18eb
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-04 12:19:00 +0530

    addition of file contain duplicate

---
+++ containsDuplicate.java
@@ -0,0 +1,26 @@
+//217
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class containsDuplicate {
+    public static boolean duplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
+        {
+                if(nums[i]==nums[i-1])
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=ob.nextInt();
+        } 
+        System.out.println(duplicate(arr));
+        ob.close();
+    }
+}

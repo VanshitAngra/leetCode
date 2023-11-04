24f3431a9282eece3ea40903b1723970e2a363ce
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-04 12:29:37 +0530

    addition of file missingNumber

---
+++ missingNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class missingNumber {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] arr=new int[n];
+        int s=n*(n-1)/2;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=ob.nextInt();
+            sum+=arr[i];
+        }
+        System.out.println(s-sum);
+        ob.close();
+    }
+}

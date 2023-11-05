63514a548ab52538db29c9cc9bbcfda12b3c9e75
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-05 23:16:24 +0530

    addition of file moving zeros

---
+++ moveZeros.java
@@ -0,0 +1,30 @@
+//283
+
+import java.util.Scanner;
+
+public class moveZeros {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] nums=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=ob.nextInt();
+        }
+        ob.close();
+        if (nums.length == 1) {
+            return;
+        }
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[a] = nums[i];
+                a++;
+            }
+        }
+         while(a<nums.length){
+            nums[a] = 0;
+            a++;
+        }
+    }
+}

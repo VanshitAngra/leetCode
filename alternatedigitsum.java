d2e9cc866138cf7ebeff8e774c034bb87bf41676
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-11 00:10:03 +0530

    addition of file alternateDigitSum

---
+++ alternatedigitsum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class alternatedigitsum {
+    public static int alternateDigitSum(int n) {
+        int sum=0,c=1;
+        String s=n+"";
+        if(s.length()%2==0)
+        {
+            c=-1;
+        }
+        while(n>0)
+        {
+            int r=n%10;
+            sum+=(r*c);
+            n/=10;
+            c*=-1;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        ob.close();
+        System.out.println(alternateDigitSum(n));
+    }
+}

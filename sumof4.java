50d29da2e40eca2c45791b53915fd0da5bb6796d
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-13 00:18:48 +0530

    addition of file sum of foyr digits after spliting it

---
+++ sumof4.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sumof4 {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int num=ob.nextInt();
+        ob.close();
+        int[] a=new int[4];
+        for(int i=0;i<4;i++)
+        {
+            a[i]=num%10;
+            num/=10;
+        }
+        Arrays.sort(a);
+        System.out.println((a[0]*10+a[2])+(a[1]*10+a[3]));
+    }
+}

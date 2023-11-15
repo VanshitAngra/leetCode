0dede26a4cc987a7193ef4268990616ef339852a
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-15 19:37:26 +0530

    addition of file difference

---
+++ difference.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class difference {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] nums=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=ob.nextInt();
+        }
+        ob.close();
+        int sum=0,sum1=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+            while(nums[i]>0)
+            {
+                sum1+=(nums[i]%10);
+                nums[i]/=10;
+            }
+            
+        }
+        System.out.println(Math.abs(sum-sum1));
+    }
+}

f3064f348784c83ba2da4755b0193eb73f199a17
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-19 00:04:18 +0530

    addition of file check

---
+++ check.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class check {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        String s=ob.next();
+        s+="{";
+         List<Integer> n=new ArrayList<>();
+        char[] c=s.toCharArray();
+        Arrays.sort(c);
+        int a=0;
+        System.out.println(String.valueOf(c));
+        ob.close();
+        for(int i=0;i<c.length-1;i++)
+        {
+
+            if(c[i]==c[i+1])
+            {
+                a++;
+                System.out.println(a);
+                continue;
+            }
+            else
+            {
+            n.add(a);
+            a=0;
+
+            }
+        }
+        System.out.println(String.valueOf(n));
+        for(int i=0;i<n.size()-1;i++)
+        {
+            if(n.get(i)!=n.get(i+1))
+            {
+                System.out.println(false);
+                return;
+
+            }
+        }
+        System.out.println(true);
+    }
+}

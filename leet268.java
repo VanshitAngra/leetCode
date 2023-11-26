8b68e7a12d9d1c50971c351001616ddd689c9945
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-27 00:02:40 +0530

    renaming of file missingNumber to leet268

--- leet268.java
+++ leet268.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-
-public class missingNumber {
+//268
+public class leet268 {
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         int n=ob.nextInt();

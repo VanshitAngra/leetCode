2ed5ab397bc49b68b72fcb6fae9eed12534fb606
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-27 00:09:51 +0530

    renaming and modificatin file valid palindrome

--- leet125.java
+++ leet125.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 /**
  * validPalindrome
  */
-public class validPalindrome {
+public class leet125 {
 
     public static void main(String[] args) {
-        String s="0P";
+        Scanner ob=new Scanner(System.in);
+        String s=ob.nextLine();
+        ob.close();
             int l=s.length();
             String s1="";
             for(int i=0;i<l;i++)
@@ -14,12 +18,20 @@ public class validPalindrome {
             else if(s.charAt(i)>=65&&s.charAt(i)<=90)
             s1+=(char)(s.charAt(i)+32);
             }
-            System.out.println(s1);
+            // System.out.println(s1);
         if(s1.length()==0)
+        {
+            System.out.println("true");
+            return;
+        }
         for(int i=0;i<=s1.length()/2;i++)
         {
             if(s1.charAt(i)!=s1.charAt(s1.length()-i-1))
-            System.out.println("false");
+            {
+                System.out.println("false");
+                return;
+            }
         }
+        System.out.println("true");
     }
 }

9d4006a57543b93144e12e0e05a11cbc2c4e06bc
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-26 23:14:43 +0530

    addition of file leet 541

---
+++ leet541.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class leet541 {
+    public static void reverse(char[] arr,int i,int j)
+    {
+        while(i<j)
+        {
+            char t=arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
+            i++;j--;
+        }
+    }
+    public static String reverseStr(String s, int k) {
+        char[] res=s.toCharArray();
+        for(int i=0;i<s.length();i+=(2*k))
+        {
+            int j=Math.min(i+k-1,s.length()-1);
+            reverse(res,i,j);
+        }
+        return String.valueOf(res);
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        String s=ob.next();
+        int n=ob.nextInt();
+        ob.close();
+        System.out.println(reverseStr(s,n));
+    }
+}

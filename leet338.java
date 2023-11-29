8399cb0ce2af445db8b8c09d8dedf299dab88e37
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-30 01:50:12 +0530

    renaming of file countingbits to leet338

--- leet338.java
+++ leet338.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //338
-public class countingBits {
+public class leet338 {
         public static void main(String[] args) {
             Scanner ob=new Scanner(System.in);
             int n=ob.nextInt();

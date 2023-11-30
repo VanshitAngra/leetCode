b22f3ca2ddea26f5b52dce6967c01964951c17c3
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-30 16:27:32 +0530

    modification in file leet2433

--- leet2433.java
+++ leet2433.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class leet2433 {
-    
+    public static int[] findArray(int[] pref) {
+        int[] ans=new int[pref.length];
+        ans[0]=pref[0];
+        for(int i=1;i<pref.length;i++)
+        {
+            ans[i]=pref[i]^pref[i-1];
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=ob.nextInt();
+        }
+        System.out.println(Arrays.toString(findArray(arr)));
+        ob.close();
     }
 }

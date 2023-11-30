29f6ffe3d39084d3ff9af1602dcb1a3f7ad4dc9b
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-11-30 16:23:33 +0530

    modification in file leet961

--- leet961.java
+++ leet961.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class leet961 {
+    public static int repeatedNTimes(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            return nums[i];
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=ob.nextInt();
+        }
+        System.out.println(repeatedNTimes(arr));
+        ob.close();
     }
 }

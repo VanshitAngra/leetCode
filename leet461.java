72f0f867cbda47d86241cf5f4b2f22569b659c8f
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-03 09:00:11 +0530

    modification in file leet461

--- leet461.java
+++ leet461.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class leet461 {
+    public static int hammingDistance(int x, int y) {
+        int c=0;
+        int a=x^y;
+        String s=Integer.toBinaryString(a);
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='1')
+            c++;
+        }
+
+        return c;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int x=ob.nextInt();
+        int y=ob.nextInt();
+        System.out.println(hammingDistance(x, y));
+        ob.close();
     }
 }

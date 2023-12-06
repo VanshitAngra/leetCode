3a61acf022f813b0db407fb72865b309375889d4
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-06 13:15:16 +0530

    modification in file leet2824

--- leet2824.java
+++ leet2824.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class leet2824 {
+    public static int countPairs(List<Integer> nums, int target) {
+        int a=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums.get(i)+nums.get(j)<target)
+                a++;
+            }
+        }
+        return a;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        List<Integer> nums=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int t=ob.nextInt();
+            nums.add(t);
+        }
+        int target=ob.nextInt();
+        System.out.println(countPairs(nums, target));
+        ob.close();
     }
 }

85773c35cd207c3ad2b469630a09fad6fca03503
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-07 12:41:14 +0530

    modification in file leet1769

--- leet1769.java
+++ leet1769.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class leet1769 {
+    public static int[] minOperations(String boxes) {
+        int n=boxes.length();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(boxes.charAt(j)=='1')
+                c+=Math.abs(i-j);
+            }
+            a[i]=c;
+        }
+        return a;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        String s=ob.nextLine();
+        System.out.println(Arrays.toString(minOperations(s)));
+        ob.close();
     }
 }

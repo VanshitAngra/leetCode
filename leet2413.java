59f65e4fdb466252a8f0bed36a74b1e8759214c5
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-08 17:48:30 +0530

    modification in file leet2413

--- leet2413.java
+++ leet2413.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class leet2413 {
+    public static int smallestEvenMultiple(int n) {
+        return n%2==0?n:n*2;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(smallestEvenMultiple(n));
+        ob.close();
     }
 }

05294f23d92f765f79ab23e59c58a169315b4467
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-11 12:16:54 +0530

    modification in file leet1689

--- leet1689.java
+++ leet1689.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class leet1689 {
+    public static int minPartitions(String n) {
+        int r=0;
+        for(int i=0;i<n.length();i++)
+        {
+            r=Math.max(r,n.charAt(i)-'0');
+        }
+        return r;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        String s=ob.next();
+        System.out.println(minPartitions(s));
+        ob.close();
     }
 }

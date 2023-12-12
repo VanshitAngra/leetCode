67ec0a052ba263ed612fadf38e156c4abcfe0b69
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-12 10:04:28 +0530

    modification in file leet2894

--- leet2894.java
+++ leet2894.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class leet2894 {
+    public static int differenceOfSums(int n, int m) {
+        int s=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%m==0)
+            s-=i;
+            else
+            s+=i;
+        }
+        return s;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int m=ob.nextInt();
+        ob.close();
+        System.out.println(differenceOfSums(n, m));
     }
 }

072f386c7d6300dd462d70193c811ac7a0db6bce
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-14 21:05:21 +0530

    modification in file l;eet2710

--- leet2710.java
+++ leet2710.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class leet2710 {
+    public static String removeTrailingZeros(String num) {
+        int a=0;
+        for(int i=num.length()-1;i>=0;i--)
+        {
+            if(num.charAt(i)=='0')
+            a++;
+            else
+            break;
+        }
+        String res="";
+        for(int i=0;i<num.length()-a;i++)
+        {
+            res+=num.charAt(i)+"";
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        String n=ob.nextLine();
+        System.out.println(removeTrailingZeros(n));
+        ob.close();
     }
 
 }

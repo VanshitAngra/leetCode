c90563a62fdf55942452ad42f0ca1aede1eeaf1c
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-15 13:04:23 +0530

    modification of file leet190

--- leet190.java
+++ leet190.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class leet190 {
-    public static void main(String[] args) {
+    public static int reverseBits(int n) {
+        if(n==0)
+        return 0;
+
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            ans <<=1;
+            ans=ans | (n&1);
+            n>>=1;
+        }
+        return ans;
+
         
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(reverseBits(n));
+        ob.close();
     }
 }

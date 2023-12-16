86f15953b6db928c245e20b8bb6388bf1668b31a
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-16 11:26:54 +0530

    modification in file leet2119

--- leet2119.java
+++ leet2119.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class leet2119 {
+    public static boolean isSameAfterReversals(int num) {
+        int a=num;
+        int rev=0;
+        while(num>0)
+        {
+            rev=rev*10+(num%10);
+            num/=10;
+        }
+        int r=0;
+        while(rev>0)
+        {
+            r=r*10+rev%10;
+            rev/=10;
+        }
+        return r==a;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(isSameAfterReversals(n));
+        ob.close();
     }
 }

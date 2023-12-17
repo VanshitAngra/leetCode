d10f2dd63d8927f3220dee630b1b9b36b1c978cb
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-17 12:12:10 +0530

    modification in file leet412

--- leet412.java
+++ leet412.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class leet412 {
+    public static List<String> fizzBuzz(int n) {
+        List<String> l=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0&&i%5==0)
+            l.add("FizzBuzz");
+            else if(i%3==0)
+            l.add("Fizz");
+            else if(i%5==0)
+            l.add("Buzz");
+            else
+            l.add(i+"");
+        }
+        return l;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        System.out.println(fizzBuzz(a));
+        ob.close();
     }
 }

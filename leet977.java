5ded09a01a38e3d6a6d98a76ef503ad8febcda20
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-19 12:33:05 +0530

    modification in file leet977

--- leet977.java
+++ leet977.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class leet977 {
+    public static int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=nums[i]*nums[i];
+        }
+         Arrays.sort(nums);
+         return nums;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        ob.close();
+        System.out.println(Arrays.toString(sortedSquares(a)));
     }
 }

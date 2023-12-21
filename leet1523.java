1cb70191efa5009e88d9c1c4341c57ba0244aa7d
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-21 10:36:19 +0530

    modification in file leet1523

--- leet1523.java
+++ leet1523.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class leet1523 {
+    public static int countOdds(int low, int high) {
+        if(low%2==0&&high%2==0)
+        return (high-low)/2;
+        else if(low%2!=0&&high%2!=0)
+        return (high-low)/2+1;
+        else
+        return (high-low+1)/2;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int l=ob.nextInt();
+        int h=ob.nextInt();
+        System.out.println(countOdds(l, h));
+        ob.close();
     }
 }

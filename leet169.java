6ede895b3ef7340d7d04c5e39a05f855fe3fba76
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-23 10:07:23 +0530

    modification in file leet169

--- leet169.java
+++ leet169.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * leet169
  */
 public class leet169 {
+     public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
 
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        System.out.println(majorityElement(a));
+        ob.close();
     }
 }

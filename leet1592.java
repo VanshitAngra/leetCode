5f7194699fbd1dfbcdeeb14991a3030f14304bc5
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-25 11:24:29 +0530

    modification in file leet1592

--- leet1592.java
+++ leet1592.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class leet1592 {
+    public staic boolean isThree(int n) {
+        int a=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            a++;
+        }
+        return a==3;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        System.out.println(isThree(a));
+        ob.close();
     }
 }

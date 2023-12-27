1201e496391bc8fe93ca0f9b17b8ed5adfb2e16c
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-27 11:24:43 +0530

    modification in file leet561

--- leet561.java
+++ leet561.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class leet561 {
+     public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2)
+        sum+=nums[i];
+        return sum;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        System.out.println(arrayPairSum(a));
+        ob.close();
     }
 }

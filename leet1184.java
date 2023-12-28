71a917132f54b5e0d21831f315a6f11f5d843797
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-28 22:37:52 +0530

    modification in file leet1184

--- leet1184.java
+++ leet1184.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class leet1184 {
-    public static void main(String[] args) {
+    public static int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        if(start>destination)
+        {int t=start;
+        start=destination;
+        destination=t;
+        }
+        int tsum=0;
+        for(int i:distance)
+        tsum+=i;
+
+        int s=0;
+        for(int i=start;i<destination;i++)
+        {
+            s+=distance[i];
+        }
+        return Math.min(s,tsum-s);
         
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] distance=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            distance[i]=ob.nextInt();
+        }
+        int start=ob.nextInt();
+        int destination=ob.nextInt();
+        ob.close();
+        System.out.println(distanceBetweenBusStops(distance, start, destination));
     }
 }

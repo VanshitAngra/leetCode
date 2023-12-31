fdf651bedc0040031b7862382ba63c14c95fcc8a
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-12-31 14:23:16 +0530

    modification in file leet1464

--- leet1464.java
+++ leet1464.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class leet1464 {
+    public static int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        ob.close();
+        System.out.println(maxProduct(a));
     }
 }

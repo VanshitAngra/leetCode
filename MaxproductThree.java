2ef58db141d44e8c7a8a47141d16a6a7e7c1e9bb
Vanshit Angra <vanshit1519.be21@chitkara.edu.in>
2024-01-01 23:48:13 +0530

    Update MaxproductThree.java

--- MaxproductThree.java
+++ MaxproductThree.java
@@ -14,6 +14,7 @@ public class MaxproductThree {
         {
             System.out.println(nums[0]*nums[1]*nums[2]);
             return;
+            //comment
 
         }
         Arrays.sort(nums);

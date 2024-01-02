84a3df8e960ebe0fea545cf3b81d96cfdf6a255a
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-02 23:28:02 +0530

    modification in file leet1672

--- leet1672.java
+++ leet1672.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class leet1672 {
+    public static int maximumWealth(int[][] accounts) {
+        int row=accounts.length;
+        int col=accounts[0].length;
+        int max=-1;
+        for(int i=0;i<row;i++)
+        {
+            int a=0;
+            for(int j=0;j<col;j++)
+            {
+                a+=accounts[i][j];
+            }
+            max=Math.max(max,a);
+        }
+        return max;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int m=ob.nextInt();
+        int[][] a=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+            a[i][j]=ob.nextInt();
+            }
+        }
+        ob.close();
+        System.out.println(maximumWealth(a));
     }
 }

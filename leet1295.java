1d90d0eb93429741ddd9a687b52d5e32ef194b8b
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-03 15:30:14 +0530

    modification in  file leet1295

--- leet1295.java
+++ leet1295.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class leet1295 {
+    public static int findNumbers(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int a=0;
+            while(nums[i]>0)
+            {
+                nums[i]/=10;
+                a++;
+            }
+            if(a%2==0)
+            c++;
+        }
+        return c;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        ob.close();
+        System.out.println(findNumbers(a));
     }
 }

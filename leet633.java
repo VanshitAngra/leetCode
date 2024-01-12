8902aa0d76348524e9155f8b98b30e2ae2664af7
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-12 16:42:48 +0530

    modification in file leet633

--- leet633.java
+++ leet633.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class leet633 {
+    public static boolean judgeSquareSum(int c) {
+        int l=0;long r=(long)Math.sqrt(c);
+        while(l<=r)
+        {
+            if((l*l)+(r*r)==c)
+            return true;
+            else if((l*l)+(r*r)<c)
+            l++;
+            else
+            r--;
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(judgeSquareSum(n));
+        ob.close();
     }
 }

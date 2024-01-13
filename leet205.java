f71a2d1de685b1636d1a23787293110a14048432
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-13 23:30:54 +0530

    modification in file leet205

--- leet205.java
+++ leet205.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class leet205 {
+    public static boolean isIsomorphic(String s, String t) {
+        int[] map1=new int[200];
+        int[] map2=new int[200];
+        if(s.length()!=t.length())
+        return false;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+            return false;
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        String a=ob.nextLine();
+        String b=ob.nextLine();
+        System.out.println(isIsomorphic(a, b));
+        ob.close();
     }
 }

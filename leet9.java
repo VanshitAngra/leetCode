7d48c7ce06db83752868fc7525524a3d3adac0d9
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-16 10:23:48 +0530

    modification in file leet9

--- leet9.java
+++ leet9.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class leet9 {
+    public static boolean isPalindrome(int x) {
+        int a=x;
+        int b=0;
+        double sum=0;
+        while(x>0)
+        {
+            b=x%10;
+            sum=(sum*10)+b;
+            x/=10;
+        }
+        return a==sum;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(isPalindrome(n));
+        ob.close();
     }
 }

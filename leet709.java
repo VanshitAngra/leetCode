b89037b012b1ee54fd6065bd74b2e7d71c4c5644
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-18 14:52:59 +0530

    modification in file leet709

--- leet709.java
+++ leet709.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class leet709 {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        String s=ob.nextLine();
+        System.out.println(toLowerCase(s));
+        ob.close();
     }
 }

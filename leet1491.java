b0b14202d095ac125a70f3e93295a307eac44edd
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-19 14:53:49 +0530

    modification in file leet704

--- leet1491.java
+++ leet1491.java
@@ -14,7 +14,7 @@ public class leet1491 {
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         int n=ob.nextInt();
-        int[][] a=new int[n];
+        int[] a=new int[n];
         for(int i=0;i<n;i++)
         {
             a[i]=ob.nextInt();
--- leet704.java
+++ leet704.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class leet704 {
+    public static int search(int[] nums, int target) {
+        int l=0;int r=nums.length;
+        int mid=l+(r-l)/2;
+        while(l<r)
+        {
+            mid=l+(r-l)/2;
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[mid]>target)
+            r--;
+            else
+            l++;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        int t=ob.nextInt();
+        System.out.println(search(a, t));
+        ob.close();
     }
 }

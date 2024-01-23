89a1160ffffd12a131257d33c20c9b89edaaad04
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-23 18:28:53 +0530

    renaming of file leet326

--- leet326.java
+++ leet326.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class powerOfthree {
+public class leet326 {
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         int n=ob.nextInt();

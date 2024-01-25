73876c6148d2fb1b99fcd3e957e08a20d5eb3e65
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-01-25 21:38:59 +0530

    addition of file leet29

---
+++ leet29.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class leet29 {
+    public static int divide(int dividend, int divisor) {
+        if(dividend==-2147483648 && divisor == -1)return --dividend;
+       return dividend/divisor;
+   }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+
+    }
+}

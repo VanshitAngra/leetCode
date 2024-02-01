214ce5c38f8c01cf3f2589999229663293a2b11e
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-02-01 16:10:57 +0530

    additon of file leet33

---
+++ leet33.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class leet33 {
+    
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        int t=ob.nextInt();
+        // System.out.println(search(a,t));
+        // ob.close();
+    }
+}

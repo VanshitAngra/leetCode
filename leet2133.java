f268e7e2a9947736da34b9b358402b7f2840eb7c
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-02-10 19:21:31 +0530

    modification in file leet2133

--- leet2133.java
+++ leet2133.java
@@ -1,8 +1,43 @@
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class leet2133 {
-   
+    public static boolean checkValid(int[][] matrix) {
+        int l=matrix.length;
+        for(int i=0;i<l;i++) 
+        {
+            HashSet<Integer> s=new HashSet<>();
+            for(int j=0;j<l;j++)
+            {
+                s.add(matrix[i][j]);
+            }
+            if(s.size()!=l)
+            return false;
+        }
+        for(int i=0;i<l;i++) 
+        {
+            HashSet<Integer> s=new HashSet<>();
+            for(int j=0;j<l;j++)
+            {
+                s.add(matrix[j][i]);
+            }
+            if(s.size()!=l)
+            return false;
+        }
+        return true;
+     }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int m=ob.nextInt();
+        int[][] nums=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            nums[i][j]=ob.nextInt();
+        }
+        ob.close();
+        System.out.println(checkValid(nums));
+    }
     }
 }

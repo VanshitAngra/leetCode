ddf7ac94f1eba8efa95c95d679fb0fbc02a40189
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-02-27 20:03:08 +0530

    modification of file leet204

--- leet204.java
+++ leet204.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class leet204 {
+    public static int countPrimes(int n) {
+        boolean[] notPrime=new boolean[n];
+        int c=0;
+        for(int i=2;i<n;i++)
+        {
+            if(notPrime[i]==false)
+            c++;
+            for(int j=2;i*j<n;j++)
+            {
+                notPrime[i*j]=true;
+            }
+        }
+        return c;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob =new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(countPrimes(n));
+        ob.close();
     }
 }

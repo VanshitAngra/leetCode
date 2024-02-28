071f01310e24ecf61ccff7e499d0c41983a93dda
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-02-28 23:41:03 +0530

    modification in  file leet371

--- leet371.java
+++ leet371.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class leet371 {
+    public static int getSum(int a, int b) {
+        while(b!=0)
+        {
+            int carry=a&b;
+            a^=b;
+            b=carry<<1;
+        }
+        return a;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        int b=ob.nextInt();
+        System.out.println(getSum(a, b));
+        ob.close();
     }
 }

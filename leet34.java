60f9605653d75700527bdb18d1049c98f122e3a4
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-04 12:13:32 +0530

    modification in file leet34

--- leet34.java
+++ leet34.java
@@ -1,5 +1,38 @@
 //**************** */
 public class leet34 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] a={-1,-1};
+        int l=0,r=nums.length-1;
+        int m=l+(r-l)/2;
+        while(l<=r)
+        {
+            m=l+(r-l)/2;
+            if(nums[m]==target)
+            {
+                a[0]=m;
+                r=m-1;
+            }
+            else if(nums[m]<target)
+            l=m+1;
+            else
+            r=m-1;
+        }
+        l=0;r=nums.length-1;
+        while(l<=r)
+        {
+            m=l+(r-l)/2;
+            if(nums[m]==target)
+            {
+                a[1]=m;
+                l=m+1;
+            }
+            else if(nums[m]<target)
+            l=m+1;
+            else
+            r=m-1;
+        }
+        return a;
+    }
     public static void main(String[] args) {
         
     }

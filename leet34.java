7f5e4585ad7fd974680be110d9d12b06d8d293d8
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-04 12:15:24 +0530

    modification in file leet34

--- leet34.java
+++ leet34.java
@@ -1,6 +1,10 @@
 //**************** */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class leet34 {
-    public int[] searchRange(int[] nums, int target) {
+    public static int[] searchRange(int[] nums, int target) {
         int[] a={-1,-1};
         int l=0,r=nums.length-1;
         int m=l+(r-l)/2;
@@ -34,6 +38,15 @@ public class leet34 {
         return a;
     }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        int t=ob.nextInt();
+        ob.close();
+        System.out.println(Arrays.toString(searchRange(a, t)));
     }
 }

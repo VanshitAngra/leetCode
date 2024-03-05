642227709fe2aaafb4adf23534bce2325728faae
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-05 17:47:13 +0530

    modification in file leet442

--- leet442.java
+++ leet442.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class leet442 {
-    public List<Integer> findDuplicates(int[] nums) {
+    public static List<Integer> findDuplicates(int[] nums) {
         List<Integer> li=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
@@ -28,6 +30,14 @@ public class leet442 {
         return li;
     }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        ob.close();
+        System.out.println(Arrays.toString(findDuplicates(a)));
     }
 }

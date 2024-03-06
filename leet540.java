94cc0dddabeadd549951360c123643b3850b9cd6
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-06 17:32:23 +0530

    modification in file leet540

--- leet540.java
+++ leet540.java
@@ -1,4 +1,18 @@
 public class leet540 {
+    public int singleNonDuplicate(int[] nums) {
+        int l=0;int r=nums.length-1;
+        while(l<r)
+        {
+            int m=l+(r-l)/2;
+            if(m%2==1)
+            m--;
+            if(nums[m]!=nums[m+1])
+            r=m;
+            else
+            l=m+2;
+        }
+        return nums[l];
+    }
     public static void main(String[] args) {
         
     }

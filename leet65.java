bc5746a83cfadcd825b95bf08ec8886d2152fb56
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-07 23:19:45 +0530

    modification in file leet65

--- leet65.java
+++ leet65.java
@@ -1,4 +1,40 @@
 public class leet65 {
+    public boolean isNumber(String s) {
+        boolean num=false;
+        boolean exp=false;
+        boolean dot=false;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.chatAt(i);
+            if(c>='0'&&c<='9')
+            num=true;
+            else if (c=='e' || c=='E')
+            {
+                if(exp||!num)
+                return false;
+
+                exp=true;
+                num=false;
+            }   
+            else if (c=='.')
+            {
+                if(dot || exp)
+                return false;
+                dot =true;
+            }
+             else if(c == '-' || c == '+')
+            {
+                if(i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        return num;
+    }
     public static void main(String[] args) {
         
     }

45907bd9460d638d26238029efba31e4f336f70c
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-10 02:15:43 +0530

    modification in file leet477

--- leet477.java
+++ leet477.java
@@ -1,4 +1,14 @@
 public class leet477 {
+    public int totalHammingDistance(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            for(int j=i+1;j<nums.length;j++){
+                sum+=Integer.bitCount(nums[i]^nums[j]);
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         
     }

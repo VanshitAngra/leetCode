8126e83d08b2003efbf141a1d3237d1682bc54de
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-03-12 17:23:06 +0530

    modification in file leet151

--- leet151.java
+++ leet151.java
@@ -1,4 +1,16 @@
 public class leet151 {
+    public String reverseWords(String s) {
+        String[] arr=s.trim().split("\\s+");
+        String a="";
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            if(i==0)
+            a+=arr[i];
+            else
+            a+=arr[i]+" ";
+        }
+        return a;
+    }
     public static void main(String[] args) {
         
     }

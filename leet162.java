282a0d80797be3c4dcc1923a3d36e97371eb7ef6
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-05-07 17:27:50 +0530

    modification of file leet162

--- leet162.java
+++ leet162.java
@@ -1,4 +1,16 @@
 public class leet162 {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1)
+        return 0;
+        if(nums[nums.length-2]<nums[nums.length-1])
+        return nums.length-1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<nums[i-1])
+            return i-1;
+        }
+        return 0;
+    }
     public static void main(String[] args) {
         
     }

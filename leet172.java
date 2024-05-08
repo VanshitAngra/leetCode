167388e2c5180def8792ce701ae3bb91a7a78c63
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-05-08 12:30:08 +0530

    modification in file leet172

--- leet172.java
+++ leet172.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class leet172 {
+    public int trailingZeroes(int n) {
+        int c=0;
+        while(n>0)
+        {
+            n/=5;
+            c+=n;
+        }
+        return c;
+    }
     public static void main(String[] args) {
-        
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        System.out.println(trailingZeroes(n));
+        ob.close();
     }
 }
